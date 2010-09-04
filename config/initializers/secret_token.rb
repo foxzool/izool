3bac61d33885d8a88f4f2fdf07bdcae7eea47ef6
ZoOL <zhooul@gmail.com>
2010-09-04 15:12:15 +0800

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Izool::Application.config.secret_token = 'b2dc260320fd773dee01adb2ebd99a90276b9cfa8ee652ef3bb300980ec29c03ec1f87c8f291bc204756c041e733332cbdbb0cfab7f3f851cedfd9e0bf945930'
